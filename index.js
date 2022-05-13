ce12c59d29975f81fbe36e47704f5281acf78e01
fradjmili <fradj.mili@esprit.tn>
2022-05-13 12:14:49 +0200

    setting server

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const userRoutes = require('./routes/user.routes');
+const eventRoutes = require('./routes/event.routes')
+const favoriteRoutes =require('./routes/favorite.routes')
+const bodyParser= require('body-parser')
+
+const app = express();
+const PORT = process.env.PORT || 3000
+const cors = require('cors');
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/../client/public"));
+app.use(bodyParser.json({limit: '50mb'}));
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function () {
+  console.log("listening on port 3000!");
+});
