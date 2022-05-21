371d17f5de12c81a8befbb5cfdc0b3efd4fdc36d
unknown <brkhayreddine@gmail.com>
2022-05-21 13:42:24 +0100

    admin signin

--- controllers/admin.controllers.js
+++ controllers/admin.controllers.js
@@ -1,12 +1,22 @@
 var db = require("../database-mysql");
 
+var selectAllsp = function (req, res) {
+    db.query("SELECT owner_name FROM sp.owner_name", (err, items, fields) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(items);
+      }
+    });
+  };
+
 var adminSignIn=(req,res)=>{
     const email=req.body.email
 const password=req.body.password
 const sqlSel=`SELECT * FROM admin WHERE  email=? AND password=? `
 db.query(sqlSel,[email,password],(err,result)=>{
     if(err){
-        res.send(err)
+        res.send('error : ',err)
     }
     if(result.length>0){
         res.send(["succesfully connected",result])
@@ -15,3 +25,8 @@ db.query(sqlSel,[email,password],(err,result)=>{
     }
 })
 }
+
+module.exports = { 
+    adminSignIn,
+    selectAllsp
+ };
--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
-
+const adminRoutes = require('./routes/admin.router')
 const bodyParser= require('body-parser')
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = 3000
 const cors = require('cors');
+var admins = require('./database-mysql');
 
-app.use(cors())
+app.use(cors({origin:'*'}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/../client/public"));
 app.use(bodyParser.json({limit: '50mb'}));
-
+app.use("/api/admins", adminRoutes);
+ 
 
 
 
