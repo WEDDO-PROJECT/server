42085b83173910b51198168748ff9fd6e8f964fd
fradjmili <fradj.mili@esprit.tn>
2022-05-23 17:01:57 +0200

    smsBack

--- controllers/ServiceProvider.controller.js
+++ controllers/ServiceProvider.controller.js
@@ -18,9 +18,70 @@
         
     }
 
+    const Register  =(req, res) =>{
+        const email = req.body.email;
+        const name = req.body.name;
+        const password = req.body.password;
+       // const confirme_Password = req.body.confirme_Password;
+        const tel_number = req.body.tel_number;
+        const cin =req.body.cin;
+        const status="pending";
+        const description=req.body.description
+      
+        const sql =
+          "INSERT INTO `SP` (`owner_name`, `email`, `password`,`cin`,`tel_number`,`status` ,`description`) VALUES (?, ?, ?, ?);";
+        const checkE = "SELECT `email` FROM weddo.`SP` where `email` = ?;";
+        db.query(checkE, [email], (err, result) => {
+          if (err) {
+            res.send({ err: err });
+          } else {
+            if (result.length > 0) {
+              res.send({
+                message: "User already have an accout assigned to this Email!!!",
+              });
+            } else {
+              db.query(sql, [name, email, password,cin, tel_number,status,description], (err, result) => {
+                if (err) {
+                  console.log("user is not added!!!");
+                } else {
+                  res.send(result);
+                  console.log("Registered successfully !!!");
+                }
+              });
+            }
+          }
+        });
+      }
 
 
+    //   app.post("/login", function (req, res) {
+    //     const email = req.body.email;
+    //     const password = req.body.password;
+      
+    //     const sql2 = "select * from `user` where `email` = ? AND `password` = ?;";
+    //     db.query(sql2, [email, password], (err, result) => {
+    //       if (err) {
+    //         res.send({ err: err });
+    //         console.log("user not found!!");
+    //       }
+    //       if (result.length > 0) {
+    //         res.send(result);
+    //       } else {
+    //         res.send({ message: "Wrong username/password combination!" });
+    //       }
+    //     });
+    //   });
+    //   app.post("/ForgotPassword",(req,res)=>{
+    //     const email = req.body.email;
+    //   const sql3 = "select * from `user` where `email` = ?";
+    //   db.query(sql3,[email],(err,res)=>{
+    //     if(err) {
+    //       res.send()
+    //     }
+    //   })
+    //   })
 module.exports = { 
     AuthWithPhone,
+    Register,
 };
     
--- routes/SPRouter.js
+++ routes/SPRouter.js
@@ -2,5 +2,5 @@ const controller =require('../controllers/ServiceProvider.controller')
 const router = require('express').Router();
 
 router.post('/AuthWithPhone',controller.AuthWithPhone);
-
+router.post('/Register',controller.Register)
 module.exports = router;
