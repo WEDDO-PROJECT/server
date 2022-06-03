dfc348cf9656a768d4730e113bbc28d12a090224
fradjmili <fradj.mili@esprit.tn>
2022-06-03 12:41:29 +0200

    stock image in database

--- database-mysql/env.js
+++ database-mysql/env.js
@@ -1,3 +1,6 @@
 module.exports = {
     myPassword: "root",
+    CLOUDINARY_CLOUD_NAME : "pentagon",
+    CLOUDINARY_API_key : "941657449646847",
+    CLOUDINARY_API_SECRET : "ObUg5NMDETrA3cqTEapjq4LD1AE",
 }
--- routes/SPRouter.js
+++ routes/SPRouter.js
@@ -1,4 +1,6 @@
 const controller =require('../controllers/ServiceProvider.controller')
+
+const controllerImage =require('../controllers/ImageController')
 const controllerSalle =require('../controllers/SalleController.js')
 const router = require('express').Router();
 
@@ -6,6 +8,8 @@ router.post('/AuthWithPhone',controller.AuthWithPhone);
 router.post('/Register',controller.Register)
 router.post('/login',controller.login)
 router.post('/addSalle',controllerSalle.AddSalle)
+router.post('/AddImage',controllerImage.AddImage)
+router.get('/getimages/:id',controllerImage.selectImage)
 router.get('/SelectSalle',controllerSalle.SelectSalle)
 router.get('/all',controller.selectAll)
 router.get('/AllServiceProvider',controller.selectAllSPRoom)
